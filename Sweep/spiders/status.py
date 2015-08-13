7617acfe080c136ee9f44c194989c3510816cdf6
AnthonyBobsin <bobsinj@gmail.com>
2015-08-13 10:13:06 -0400

    Adds implementation to send keys to an input

--- Sweep/spiders/status.py
+++ Sweep/spiders/status.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class StatusSpider(scrapy.Spider):
     name = "status"
@@ -19,4 +22,9 @@ class StatusSpider(scrapy.Spider):
         el = self.driver.find_element_by_xpath("//button[contains(@class, 'main-butt')]")
         if el:
             el.click()
+        form = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "fill-me-in"))
+        )
+        form.send_keys("hello world")
+
         # self.driver.close()
