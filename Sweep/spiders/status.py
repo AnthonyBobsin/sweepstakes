8547b875cb5fe858e0b5ab757512f533085ffb41
AnthonyBobsin <bobsinj@gmail.com>
2015-08-14 20:18:22 -0400

    Crawler now loops through each status and can retweet

--- Sweep/spiders/status.py
+++ Sweep/spiders/status.py
@@ -21,6 +21,41 @@ class StatusSpider(scrapy.Spider):
         self.driver = webdriver.Chrome()
         self.statuses = statuses
 
+    def parse(self, response):
+        self.login(response)
+        self.wait_for(self.page_has_loaded)
+        self.retweet_statuses()
+        # self.driver.close()
+
+    def login(self, response):
+        self.driver.get(response.url)
+        self.set_old_page()
+        email_form = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//input[contains(@id, 'signin-email')]"))
+        )
+        password_form = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//input[contains(@id, 'signin-password')]"))
+        )
+        email_form.send_keys(config.account['email'])
+        password_form.send_keys(config.account['password'])
+        login_button = self.driver.find_element(By.XPATH, "//button[contains(@class, 'flex-table-btn')]")
+        if login_button:
+            login_button.click()
+
+    def retweet_statuses(self):
+        # self.driver.get(self.status_url(self.statuses[0]))
+
+        for s in self.statuses:
+            # self.wait_for(self.page_has_loaded)
+            self.driver.get(self.status_url(s))
+            retweet_button = self.driver.find_element(By.CSS_SELECTOR, ".opened-tweet .Icon--retweet")
+            if retweet_button:
+                retweet_button.click()
+                time.sleep(0.5) # wait for modal
+                action_button = self.driver.find_element(By.CSS_SELECTOR, ".retweet-action")
+                if action_button:
+                    action_button.click()
+
     def status_url(self, status):
         # TODO: Might be able to get URL from Status class
         return "http://www.twitter.com/%s/status/%s" % (status['user'], status['id'])
@@ -44,29 +79,6 @@ class StatusSpider(scrapy.Spider):
             'Timeout waiting for {}'.format(condition_function.__name__)
         )
 
-    def login(self, response):
-        self.driver.get(response.url)
-        self.set_old_page()
-        email_form = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//input[contains(@id, 'signin-email')]"))
-        )
-        password_form = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//input[contains(@id, 'signin-password')]"))
-        )
-        email_form.send_keys(config.account['email'])
-        password_form.send_keys(config.account['password'])
-        login_button = self.driver.find_element_by_xpath("//button[contains(@class, 'flex-table-btn')]")
-        if login_button:
-            login_button.click()
-
-        self.wait_for(self.page_has_loaded)
-        self.driver.get(self.status_url(self.statuses[0]))
-
-
-    def parse(self, response):
-        self.login(response)
-        # self.driver.close()
-
 
 # EXAMPLES
 # el = self.driver.find_element_by_xpath("//button[contains(@class, 'main-butt')]")
@@ -76,3 +88,5 @@ class StatusSpider(scrapy.Spider):
 #     EC.presence_of_element_located((By.CLASS_NAME, "fill-me-in"))
 # )
 # form.send_keys("hello world")
+# retweet_button = $('span.Icon--retweet')[0]
+# follow_button = $('button.user-actions-follow-button')
